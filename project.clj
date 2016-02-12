02e29ea2f32569e308b58c7d872d282ab83177d2
Przemek <przemek@delacien>
2016-02-12 13:22:46 +0000

    close socket in send-udp

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fudi-clj "0.1.0-SNAPSHOT"
+(defproject clj-fudi "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
