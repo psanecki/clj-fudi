92773bd0c2821c97d100dd27557497a130c98b4a
Przemek <przemek@delacien>
2016-04-22 15:55:47 +0100

    add persistent tcp server

--- src/clj_fudi/core.clj
+++ src/clj_fudi/core.clj
@@ -3,6 +3,7 @@
             [clojure.string :as str])
   (:import [java.net
             Socket
+            ServerSocket
             DatagramSocket
             DatagramPacket
             InetSocketAddress]
@@ -11,51 +12,28 @@
             DataInputStream
             StringWriter]))
 
+(defn- a->str
+  [a]
+  (cond
+    (number? a) (str (float a))
+    (keyword? a) (name a)
+    (instance? java.lang.Boolean a) (if a "1" "0")
+    (string? a) a
+    :else ""))
 
-
-(defn dict->fudi-str-seq [obj-msg]
+(defn dict->fudi-str-seq
+  [obj-msg]
   (for [k (keys obj-msg)]
     (let [message (k obj-msg)
           message (if (coll? message)
                     (let [message-str
-                          (map
-                           #(cond
-                              (number? %) (str (float %))
-                              (keyword? %) (name %)
-                              (instance? java.lang.Boolean %) (if % "1" "0")
-                              (string? %) %
-                              :else "")
-                           message)]
+                          (map a->str message)]
                       (->>
                        message-str
                        (interpose " ")
-
                        (reduce str "list ")))
-                    ;; (if (number? (first message))
-                    ;;   (->>
-                    ;;    (interpose " " (map (comp str float) message))
-                    ;;    (reduce str "list "))
-                    ;;   (->>
-                    ;;    (interpose " "  message)
-                    ;;    (reduce str "list ")))
-                    ;; else
-                    (if (number? message)
-                      (str (float message))
-                      message))]
-      (reduce str (name k) [" " message ";\n"]))
-    ;; (apply str (name k) " "
-    ;;      (let [message (k obj-msg)]
-    ;;        (if (coll? message)
-    ;;          (let [messages (k obj-msg)
-    ;;                messages (if (number? (first messages))
-    ;;                           (map float messages)
-    ;;                           (map str messages))]
-    ;;            (apply str "list"
-    ;;                   " "
-    ;;                   (reduce str (interpose " " messages))))
-    ;;          message))
-    ;;      ";\n")
-    ))
+                   (a->str message))]
+      (str/join  [(name k) " " message ";\r\n"]))))
 
 
 
@@ -76,9 +54,49 @@
     (doseq [packet packets] (.send socket packet))
     (.close socket)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- receive
+  "Read a line of textual data from the given socket"
+  [socket]
+  (.readLine (io/reader socket)))
+
+(defn- send1
+  "Send the given string message out over the given socket"
+  [socket msg]
+  (let [writer (io/writer socket)]
+    (prn (type msg))
+    (prn msg)
+    (.write writer msg)
+    (.flush writer)))
+
+(defn serve-tcp
+  "Handler must return map of dictionaries"
+  [handler port]
+  (with-open [server-sock (ServerSocket. port)
+              sock (.accept server-sock)]
+    (let [msg-in (receive sock)
+          msg-out (-> msg-in handler dict->fudi-str-seq)
+          writer (io/writer sock)]
+      (doseq [m msg-out] (.write writer m))
+      (.flush writer))))
+
+(defn serve-persistent-tcp [handler port]
+  (let [running (atom true)]
+    (future
+      (with-open [server-sock (ServerSocket. port)]
+        (while @running
+          (with-open [sock (.accept server-sock)]
+            (let [msg-in (receive sock)
+                  msg-out (-> msg-in handler dict->fudi-str-seq)
+                  writer (io/writer sock)]
+              (doseq [m msg-out] (.write writer m))
+              (.flush writer))))))
+    running))
 
 
 
+
+;;;;;;;;;;;;;;;
 (defn send-tcp
   [obj-msg host port]
   ;;   ex. (send-tcp {:a 2 :b [2]}  :localhost 3000)
@@ -87,37 +105,20 @@
           out (.getOutputStream socket)
           dos (new DataOutputStream out)
           messages (dict->fudi-str-seq obj-msg)
-          ;;_ (prn messages)
-          buffer (.getBytes (reduce str messages))
+          buffer (.getBytes (str/join messages))
           ]
-      (.write dos buffer 0 (alength buffer)))
+      (doto dos
+        (.write buffer 0 (alength buffer))
+         .flush
+         .close
+        ))
     (catch Exception e (.printStackTrace e))))
 
 
-;; (defn check-tcp-connection [host port]
-;;   (try
-;;     (with-open [socket (new Socket host port)]
-;;       (.isConnected socket))
-;;     (catch Exception e (.printStackTrace e))))
-
-;; (comment
-;;   (defn check-tcp-response [host port]
-;;     (with-open [sock (Socket. host port)
-;;                 writer (io/writer sock)
-;;                 reader (io/reader sock)
-;;                 response (StringWriter.)]
-;;       (doto writer (.append "isConnected? bang;") (.flush))
-;;       (io/copy reader response)
-;;       (str response)))
-;;   (check-tcp-response "localhost" 3000))
-
-;; (check-tcp-connection "localhost" 3000)
-
-
-
-(defn send-fudi [message port]
+(defn send-fudi
+  [message port]
   (with-open [sock (new Socket "localhost" port)
               writer (io/writer sock)]
     (doto writer
-      (.append message)
+      (.append (str message "\r\n"))
       (.flush))))
